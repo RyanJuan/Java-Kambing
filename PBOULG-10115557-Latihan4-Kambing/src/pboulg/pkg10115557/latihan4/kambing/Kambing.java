8e563b77b9e74e555d20dd98972b1189cafa8d18
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 20:18:45 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan4-Kambing/src/pboulg/pkg10115557/latihan4/kambing/Kambing.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan4.kambing;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 4
+ */
+public class Kambing {
+    public void tambahKambing() {
+	//Deklarasi Var lokal
+	int jumlahKambing = 0;
+
+	jumlahKambing = jumlahKambing + 5;
+	System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+	}
+}
---
+++ PBOULG-10115557-Latihan4-Kambing/src/pboulg/pkg10115557/latihan4/kambing/PBOULG10115557Latihan4Kambing.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan4.kambing;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 4
+ */
+public class PBOULG10115557Latihan4Kambing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+}
